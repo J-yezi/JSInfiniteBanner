2afe865a4c502ab32c7c67f096aa71b061a54584
jesse <jesse@yehaodeMacBook-Pro.local>
2017-06-16 15:12:26 +0800

    commit

--- Example/Example/ViewController.swift
+++ Example/Example/ViewController.swift
@@ -11,13 +11,14 @@ import JSInfiniteBanner
 
 class ViewController: UIViewController {
 
-    var bannber: JSInfiniteBanner!
-    
+    var banner: JSInfiniteBanner!
     override func viewDidLoad() {
         super.viewDidLoad()
         
-        bannber = JSInfiniteBanner(frame: CGRect(x: 0, y: 20, width: view.bounds.width, height: 200), data: ["http://pic130.nipic.com/file/20170518/12398452_110125003000_2.jpg", "http://pic129.nipic.com/file/20170508/21897230_112747358000_2.jpg", "http://pic129.nipic.com/file/20170513/24778557_175703312000_2.jpg", "http://pic129.nipic.com/file/20170515/24480651_125442112000_2.jpg"])
-        view.addSubview(bannber)
+        banner = JSInfiniteBanner(frame: CGRect(x: 0, y: 20, width: view.bounds.width, height: 200), data: ["http://pic130.nipic.com/file/20170518/12398452_110125003000_2.jpg", "http://pic129.nipic.com/file/20170508/21897230_112747358000_2.jpg", "http://pic129.nipic.com/file/20170513/24778557_175703312000_2.jpg", "http://pic129.nipic.com/file/20170515/24480651_125442112000_2.jpg"])
+        view.addSubview(banner)
+        
+        /// ["http://pic130.nipic.com/file/20170518/12398452_110125003000_2.jpg", "http://pic129.nipic.com/file/20170508/21897230_112747358000_2.jpg", "http://pic129.nipic.com/file/20170513/24778557_175703312000_2.jpg", "http://pic129.nipic.com/file/20170515/24480651_125442112000_2.jpg"]
     }
 
 
--- JSInfiniteBanner/JSInfiniteBanner.swift
+++ JSInfiniteBanner/JSInfiniteBanner.swift
@@ -11,20 +11,24 @@ import UIKit
 public class JSInfiniteBanner: UIView {
     
     fileprivate let identifier = "JSInfiniteBannerIdentifier"
-    fileprivate var data: [String]!
-    fileprivate var pages = [CGFloat]()
+    fileprivate let sectionCount: Int = 201
+    fileprivate let intervals: TimeInterval = 4
+    fileprivate var timer: Timer?
     fileprivate lazy var pageControl: UIPageControl = {
         let pageControl: UIPageControl = UIPageControl(frame: CGRect(x: 0, y: self.bounds.height - 35, width: self.bounds.width, height: 20))
         pageControl.pageIndicatorTintColor = UIColor.gray
         pageControl.currentPageIndicatorTintColor = UIColor.red
         return pageControl
     }()
-    fileprivate lazy var collectionView: UICollectionView = {
+    fileprivate lazy var layout: UICollectionViewFlowLayout = {
         let layout = UICollectionViewFlowLayout()
         layout.scrollDirection = .horizontal
         layout.minimumLineSpacing = 0
         layout.itemSize = self.bounds.size
-        let collectionView: UICollectionView = UICollectionView(frame: self.bounds, collectionViewLayout: layout)
+        return layout
+    }()
+    fileprivate lazy var collectionView: UICollectionView = {
+        let collectionView: UICollectionView = UICollectionView(frame: self.bounds, collectionViewLayout: self.layout)
         collectionView.backgroundColor = UIColor.white
         collectionView.showsVerticalScrollIndicator = false
         collectionView.showsHorizontalScrollIndicator = false
@@ -34,16 +38,32 @@ public class JSInfiniteBanner: UIView {
         collectionView.register(JSInfiniteBannerCell.self, forCellWithReuseIdentifier: self.identifier)
         return collectionView
     }()
+    public var data: [String] = [String]() {
+        didSet {
+            if data.count > 0 {
+                if data.count > 1 {
+                    pageControl.isHidden = false
+                    startTimer()
+                }else {
+                    pageControl.isHidden = true
+                    stopTimer()
+                }
+                dataSet()
+                collectionView.reloadData()
+            }
+        }
+    }
     
     public init(frame: CGRect, data: [String]) {
         super.init(frame: frame)
-        self.data = data
+        /// 在init方法里面，如果不适用kvc的方法，是不会调用didSet方法
+        self.setValue(data, forKey: "data")
         uiSet()
-        dataSet()
     }
     
     public required init?(coder aDecoder: NSCoder) {
-        fatalError("init(coder:) has not been implemented")
+        super.init(coder: aDecoder)
+        uiSet()
     }
     
 }
@@ -53,15 +73,29 @@ extension JSInfiniteBanner {
     func uiSet() {
         addSubview(collectionView)
         addSubview(pageControl)
-        
     }
     
     func dataSet() {
-        collectionView.setContentOffset(CGPoint(x: bounds.width * CGFloat(data.count * 100), y: 0), animated: true)
-        pageControl.numberOfPages = data.count
-        (0..<data.count).forEach {
-            pages.append(CGFloat($0) * bounds.width + bounds.width / 2)
+        if data.count > 1 {
+            collectionView.setContentOffset(CGPoint(x: bounds.width * CGFloat(data.count * (sectionCount - 1) / 2), y: 0), animated: false)
+            pageControl.numberOfPages = data.count
+        }else {
+            collectionView.setContentOffset(CGPoint.zero, animated: false)
         }
+    }
+    
+    func repeatScroll() {
+        collectionView.setContentOffset(CGPoint(x: collectionView.contentOffset.x + bounds.width, y: 0), animated: true)
+    }
+    
+    func startTimer() {
+        timer = Timer(timeInterval: intervals, target: self, selector: #selector(repeatScroll), userInfo: nil, repeats: true)
+        RunLoop.current.add(timer!, forMode: RunLoopMode.commonModes)
+    }
+    
+    func stopTimer() {
+        timer?.invalidate()
+        timer = nil
     }
     
 }
@@ -69,21 +103,31 @@ extension JSInfiniteBanner {
 extension JSInfiniteBanner: UICollectionViewDelegate, UICollectionViewDataSource {
     
     public func scrollViewDidScroll(_ scrollView: UIScrollView) {
-        let offset = scrollView.contentOffset.x .truncatingRemainder(dividingBy: bounds.width * CGFloat(data.count))
-        if pages.count > 0, offset > pages.last! {
-            pageControl.currentPage = 0
-        }else {
-            for (index, x) in pages.enumerated() {
-                if offset < x {
-                    pageControl.currentPage = index
-                    break
-                }
-            }
-        }
+        guard data.count > 1 else { return }
+        
+        let currentPage: Int = Int(((scrollView.contentOffset.x + layout.itemSize.width / 2) / layout.itemSize.width).truncatingRemainder(dividingBy: CGFloat(data.count)))
+        pageControl.currentPage = currentPage
+    }
+    
+    public func scrollViewWillBeginDragging(_ scrollView: UIScrollView) {
+        stopTimer()
+    }
+    
+    public func scrollViewDidEndDragging(_ scrollView: UIScrollView, willDecelerate decelerate: Bool) {
+        startTimer()
+    }
+    
+    /// 因为这种banner有取巧的嫌疑，所有如果用户停止了连续拖动后，将collectionView又设置到中间
+    public func scrollViewDidEndDecelerating(_ scrollView: UIScrollView) {
+        collectionView.setContentOffset(CGPoint(x: layout.itemSize.width * CGFloat(data.count * (sectionCount - 1) / 2 + pageControl.currentPage), y: 0), animated: false)
+    }
+    
+    public func scrollViewDidEndScrollingAnimation(_ scrollView: UIScrollView) {
+        collectionView.setContentOffset(CGPoint(x: layout.itemSize.width * CGFloat(data.count * (sectionCount - 1) / 2 + pageControl.currentPage), y: 0), animated: false)
     }
     
     public func numberOfSections(in collectionView: UICollectionView) -> Int {
-        return 201
+        return data.count > 1 ? sectionCount : 1
     }
     
     public func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
